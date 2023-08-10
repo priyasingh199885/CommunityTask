b442ff4bf3e20c78d368227cd7534f852a86d773
Klaus Haeuptle <klaus.haeuptle@sap.com>
2023-08-10 12:21:58 +0200

    :sparkles: feat(ChatBTPScripts): Add AssistantTwoWritePrompts.sh and ListAcronymsWithoutExplanation.sh
    Two new scripts have been added to the ChatBTPScripts directory. The first script, AssistantTwoWritePrompts.sh, is designed to help in creating prompts for chatgpt models to improve markdown documents. The second script, ListAcronymsWithoutExplanation.sh, is aimed at analyzing input markdown documents for acronyms and creating a list of acronyms without explanation.
    
    These scripts were added to automate and streamline the process of improving markdown documents and analyzing them for acronyms. This will save time and ensure consistency in the tasks.

---
+++ ChatBTPScripts/AssistantTwoWritePrompts.sh
@@ -0,0 +1,4 @@
+chatBTP ask -m 'Act as an assistant to write prompts for chatgpt models.
+      I am trying to improve markdown documents. I would like to develop a prompt that makes chatgpt improve a given markdown document and responds only the raw markdown as improved version so that i can directly pipe the output into a markdown file.
+      it is important that no other text is returned as the response should directly be used.
+      Suggest a prompt that leads to this result'
