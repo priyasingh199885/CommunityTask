3c9b7cd1b57fdf13125be92a2e44536d93abf2d7
Klaus Haeuptle <klaus.haeuptle@sap.com>
2023-08-10 12:25:10 +0200

    🚚✨ refactor: Rename AssistantTwoWritePrompts.sh to AssistantToWritePrompts.sh 📝✨ feat: Add ImproveADR.sh script in ChatBTPScripts
    The AssistantTwoWritePrompts.sh script was renamed to AssistantToWritePrompts.sh to correct a typographical error and improve clarity. The new name better reflects the script's purpose, which is to assist in writing prompts.
    
    A new script, ImproveADR.sh, was added to the ChatBTPScripts directory. This script is designed to improve Architecture Decision Records (ADRs) by providing style, grammar, and technical suggestions. It is expected to enhance the quality of ADRs and ensure they are written in a consistent and clear manner.

---
+++ ChatBTPScripts/ImproveADR.sh
@@ -0,0 +1,2 @@
+chatBTP ask -m 'Act as an assistant to write and improve Architecture Decision Records
+      Consider the following architecture decision record as input as input and suggest improvements style, grammar and technical suggestions. Remember to respond only in raw markdown format.' --file $0
